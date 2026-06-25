2e65638c5788024a9301ace760a55311d9ebadc4
Julio <soares.julio@academico.ifpb.edu.br>
2026-06-25 09:43:44 -0300

    feat: define modelo e schema de validacao para a entidade Galinaceo

--- models/Galinaceo.py
+++ models/Galinaceo.py
@@ -1,13 +1,14 @@
 from marshmallow import Schema, fields
 
 class Galinaceo:
-    def __init__(self, id, sist_cria, niv_terr, cod_terr, nom_terr, cl_gal, gal_total):
+    def __init__(self, id, sist_cria, niv_terr, cod_terr, nom_terr, cl_gal, nom_cl_gal, gal_total):
         self.id = id
         self.sist_cria = sist_cria
         self.niv_terr = niv_terr
         self.cod_terr = cod_terr
         self.nom_terr = nom_terr
         self.cl_gal = cl_gal
+        self.nom_cl_gal = nom_cl_gal
         self.gal_total = gal_total
 
     def toDict(self):
@@ -18,6 +19,7 @@ class Galinaceo:
             "cod_terr": self.cod_terr,
             "nom_terr": self.nom_terr,
             "cl_gal": self.cl_gal,
+            "nom_cl_gal": self.nom_cl_gal,
             "gal_total": self.gal_total
         }
 
@@ -25,7 +27,8 @@ class GalinaceoSchema(Schema):
     id = fields.Int(dump_only=True)
     sist_cria = fields.Str(required=True)
     niv_terr = fields.Str(required=True)
-    cod_terr = fields.Str(allow_none=True) # Pode ser nulo/vazio conforme o CSV
+    cod_terr = fields.Str(allow_none=True)
     nom_terr = fields.Str(required=True)
     cl_gal = fields.Str(required=True)
-    gal_total = fields.Int(required=True)
+    nom_cl_gal = fields.Str(required=True)
+    gal_total = fields.Int(allow_none=True)
